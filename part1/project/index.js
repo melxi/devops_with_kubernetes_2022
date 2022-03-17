a6d4100645c82a71bbbe68dbcd83a1c0a4838a27
melxi <magomed.futijev@gmail.com>
2022-03-17 09:07:02 +0000

    Exercise 1.05: Project v0.3

--- part1/project/index.js
+++ part1/project/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const port = process.env.PORT | 3000
 
+// Serve static assets
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send("Project v0.1")
+  res.render("index")
 })
 
 app.listen(port, () => {
