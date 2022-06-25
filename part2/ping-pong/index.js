ee6fed6d92f405eef7ec275d63bbb2ddc77d26cf
melxi <magomed.futijev@gmail.com>
2022-06-25 22:12:43 +0000

    Exercise 2.07: Stateful applications

--- part2/ping-pong/index.js
+++ part2/ping-pong/index.js
@@ -1,11 +1,50 @@
 const express = require('express')
+const { Pool, Client } = require('pg')
 const app = express()
 const port = process.env.PORT || 3002
-let counter = 0;
+const { 
+  POSTGRES_DB,
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD
+} = process.env
 
-app.get("/pingpong", (req, res) => {
+console.log(POSTGRES_DB,
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD);
+
+const client = new Client({
+  user: POSTGRES_USER,
+  host: POSTGRES_HOST,
+  database: POSTGRES_DB,
+  password: POSTGRES_PASSWORD,
+  port: POSTGRES_PORT
+})
+
+client.connect()
+
+client.query('CREATE TABLE IF NOT EXISTS pingpong (id VARCHAR(12), count INTEGER DEFAULT 0)')
+
+client.query('SELECT EXISTS(SELECT count FROM pingpong)', (err, res) => {
+  if (!res.rows[0].exists) {
+    client.query('INSERT INTO pingpong (id, count) VALUES($1, $2);', ['pingpongs', 0], (err, res) => {
+      console.log(err, res)
+    })
+  }
+})
+
+app.get("/pingpong", async (req, res) => {
+  let counter;
+  const { rows } = await client.query('SELECT count FROM pingpong WHERE id=$1', ['pingpongs'])
+
+  counter = rows[0].count;
+
   res.send(`Ping / Pongs: ${counter}`);
-  counter++;
+
+  client.query('UPDATE pingpong SET count = count + 1')
 })
 
 app.listen(port, () => {
