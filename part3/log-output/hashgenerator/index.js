f3813bebaf932d98d35e72e52fa8d4c6289a5dc6
melxi <magomed.futijev@gmail.com>
2022-07-02 12:43:24 +0000

    Exercise 3.01: Pingpong GKE

---
+++ part3/log-output/hashgenerator/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const axios = require('axios')
+const uuid = require('uuid')
+const app = express()
+const port = process.env.PORT || 3001
+const greeting = process.env.GREETING || 'Tervehdys'
+
+app.get('/', async (req, res) => {
+  const randomHash = uuid.v4()
+  const timestamp = await axios.get('http://localhost:3000')
+                               .then(res => res.data)
+                               .catch(err => err)
+
+  const counter = await axios.get('http://pingpong-svc:8089/pingpong')
+                             .then(res => res.data)
+                             .catch(err => err)
+
+  const result = `${greeting} \n${timestamp}: ${randomHash} \n${counter}`
+    
+  res.send(result)
+})
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`)
+})
---
+++ part3/project/backend/index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const { Client } = require('pg')
+const morgan = require('morgan')
+const axios = require('axios')
+const app = express()
+const port = process.env.PORT || 3001
+const { 
+  POSTGRES_DB,
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD
+} = process.env
+
+const client = new Client({
+  user: POSTGRES_USER,
+  host: POSTGRES_HOST,
+  database: POSTGRES_DB,
+  password: POSTGRES_PASSWORD,
+  port: POSTGRES_PORT
+})
+
+app.use(express.json())
+app.use(morgan('common'))
+
+client.connect()
+
+client.query('CREATE TABLE IF NOT EXISTS todos (id SERIAL PRIMARY KEY, title TEXT NOT NULL, completed BOOLEAN)')
+
+app.get('/todos', async (req, res) => {
+  const { rows } = await client.query('SELECT * FROM todos')
+  res.json(rows)
+})
+
+app.post('/todos', async (req, res) => {
+  const { title } = req.body
+
+  if (title.length > 140) {
+    res.json({ message: 'Todo is too long!' })
+  } else {
+    await client.query('INSERT INTO todos (title, completed) VALUES($1, $2);', [title, false])
+
+    res.json({ message: 'Todo added' })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`)
+})
